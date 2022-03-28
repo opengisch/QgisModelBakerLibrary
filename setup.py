1b41c7d89fe059955e0b5574613cc39b179b7a5e
signedav <david@opengis.ch>
2022-03-28 17:20:28 +0200

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="modelbaker",
-    version="0.0.3",
+    version="0.0.4",
     author="Dave Signer",
     author_email="david@opengis.ch",
     description="The full model baker core",
