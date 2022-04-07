d1b22e718a78f718224d75d88f40759408ea8bd2
signedav <david@opengis.ch>
2022-04-07 14:40:06 +0200

    find packages and exclude tests

--- setup.py
+++ setup.py
@@ -23,5 +23,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    packages=["modelbaker"],
+    packages=setuptools.find_packages(exclude=["tests"]),
 )
