1a55c8260988ec1f70848ad66b9dd16965610af2
Damiano Lombardi <damiano@opengis.ch>
2022-09-22 15:28:32 +0200

    Fix postgis query

--- modelbaker/dbconnector/pg_connector.py
+++ modelbaker/dbconnector/pg_connector.py
@@ -579,17 +579,19 @@ class PGConnector(DBConnector):
                 )
                 strength_group_by = ", META_ATTRS.attr_value"
 
-                cardinality_max_field = ", META_ATTRS.attr_value as cardinality_max"
+                cardinality_max_field = (
+                    ", META_ATTRS_CARDINALITY.attr_value as cardinality_max"
+                )
                 cardinality_max_join = """
-                            LEFT JOIN {schema}.t_ili2db_attrname AS ATTRNAME
-                             ON ATTRNAME.sqlname = KCU1.COLUMN_NAME AND ATTRNAME.{colowner} = KCU1.TABLE_NAME AND ATTRNAME.target = KCU2.TABLE_NAME
-                            LEFT JOIN {schema}.{t_ili2db_meta_attrs} AS META_ATTRS
-                             ON META_ATTRS.ilielement = ATTRNAME.iliname AND (META_ATTRS.attr_name = 'ili2db.ili.assocCardinalityMax' OR META_ATTRS.attr_name = 'ili2db.ili.attrCardinalityMax')""".format(
+                            LEFT JOIN {schema}.t_ili2db_attrname AS ATTRNAME_CARDINALITY
+                             ON ATTRNAME_CARDINALITY.sqlname = KCU1.COLUMN_NAME AND ATTRNAME_CARDINALITY.{colowner} = KCU1.TABLE_NAME AND ATTRNAME_CARDINALITY.target = KCU2.TABLE_NAME
+                            LEFT JOIN {schema}.{t_ili2db_meta_attrs} AS META_ATTRS_CARDINALITY
+                             ON META_ATTRS_CARDINALITY.ilielement = ATTRNAME_CARDINALITY.iliname AND (META_ATTRS_CARDINALITY.attr_name = 'ili2db.ili.assocCardinalityMax' OR META_ATTRS_CARDINALITY.attr_name = 'ili2db.ili.attrCardinalityMax')""".format(
                     schema=self.schema,
                     t_ili2db_meta_attrs=PG_METAATTRS_TABLE,
                     colowner="owner" if self.ili_version() == 3 else "colowner",
                 )
-                cardinality_max_group_by = ", META_ATTRS.attr_value"
+                cardinality_max_group_by = ", META_ATTRS_CARDINALITY.attr_value"
 
             cur.execute(
                 """SELECT RC.CONSTRAINT_NAME, KCU1.TABLE_NAME AS referencing_table, KCU1.COLUMN_NAME AS referencing_column, KCU2.CONSTRAINT_SCHEMA, KCU2.TABLE_NAME AS referenced_table, KCU2.COLUMN_NAME AS referenced_column, KCU1.ORDINAL_POSITION{strength_field}{cardinality_max_field}
