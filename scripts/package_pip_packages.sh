0ed4a7b535c1376635bbad1cb84cd2b3892fe35b
signedav <david@opengis.ch>
2022-12-16 09:03:47 +0100

    bump toppingmaker to 1.2.5

--- scripts/package_pip_packages.sh
+++ scripts/package_pip_packages.sh
@@ -3,7 +3,7 @@ LIBS_DIR="modelbaker/libs"
 
 DEPRECATION=("deprecation" "2.1.0")
 PGSERVICEPARSER=("pgserviceparser" "1.1.0")
-TOPPINGMAKER=("toppingmaker" "1.2.4")
+TOPPINGMAKER=("toppingmaker" "1.2.5")
 
 PACKAGES=(
   DEPRECATION[@]
