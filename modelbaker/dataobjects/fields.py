030449f76ef71a9b84c64e964ac54de16bb7ac04
Germán Carrillo <gcarrillo@linuxmail.org>
2024-06-05 17:33:33 -0500

    Use 'from __future__ import annotations' to avoid 'Layer is not defined issues'

--- modelbaker/dataobjects/fields.py
+++ modelbaker/dataobjects/fields.py
@@ -16,6 +16,8 @@
  *                                                                         *
  ***************************************************************************/
 """
+from __future__ import annotations
+
 from typing import TYPE_CHECKING
 
 from qgis.core import QgsDefaultValue, QgsEditorWidgetSetup
