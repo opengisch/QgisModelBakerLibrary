f7b926efcbfa02e05b97e560d6b6ef8c9415d2f7
Germán Carrillo <gcarrillo@linuxmail.org>
2024-06-10 10:44:02 -0500

    Include attachmentkey column when retrieving basket info

--- modelbaker/dbconnector/db_connector.py
+++ modelbaker/dbconnector/db_connector.py
@@ -287,6 +287,7 @@ class DBConnector(QObject):
                 basket_t_id
                 basket_t_ili_tid
                 topic
+                attachmentkey
                 dataset_t_id
                 datasetname
         """
