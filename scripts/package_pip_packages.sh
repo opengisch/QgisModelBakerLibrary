bfd006aa5b80a2a79776698a6fdd796ccaa36ef4
signedav <david@opengis.ch>
2026-07-13 14:30:15 +0200

    Integration of the ili2py library and BakerPyIndex to get information from the metamodel and create the enumeration or the python classes (via library) from it.

--- scripts/package_pip_packages.sh
+++ scripts/package_pip_packages.sh
@@ -5,14 +5,20 @@ DEPRECATION=("deprecation" "2.1.0")
 PGSERVICEPARSER=("pgserviceparser" "2.2.1")
 TOPPINGMAKER=("toppingmaker" "1.6.0")
 
+XSDATA=("xsdata" "26.2")
+TYPINGEXTENSIONS=("typing-extensions" "4.16.0")
+
 PACKAGES=(
   DEPRECATION[@]
   PGSERVICEPARSER[@]
   TOPPINGMAKER[@]
+  XSDATA[@]
+  TYPINGEXTENSIONS[@]
 )
 
-#create lib folder
-mkdir -p $LIBS_DIR
+#recreate lib folder
+rm -rf "$LIBS_DIR"
+mkdir -p "$LIBS_DIR"
 
 for PACKAGE in ${PACKAGES[@]}; do
   echo download and unpack ${!PACKAGE:0:1} with version ${!PACKAGE:1:1}
@@ -24,9 +30,19 @@ for PACKAGE in ${PACKAGES[@]}; do
   unzip -o "temp/*.whl" -d $LIBS_DIR
   #remove temp folder
   rm -r temp
-  #set write rights to group (because qgis-plugin-ci needs it)
-  chmod -R g+w $LIBS_DIR
 done
+
+# ili2py not yet on pypi - we get it from the repo
+COMMIT="c0e815e2fec9b5a5410633c291098de393529160"
+echo get ili2py on commit "$COMMIT"
+git clone -q https://github.com/rudert-geoinformatik/ili2py.git temp_ili2py_git
+cd temp_ili2py_git && git checkout -q "$COMMIT" && cd - > /dev/null
+mv temp_ili2py_git/src/ili2py "$LIBS_DIR/"
+rm -rf temp_ili2py_git
+echo ili2py stored
+
+#set write rights to group (because qgis-plugin-ci needs it)
+chmod -R g+w $LIBS_DIR
 
 #create the __init__.py in libs folder
 cd $LIBS_DIR
