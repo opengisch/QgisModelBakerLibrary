77ce8bac8b94117b96af712f77eb3f7061b96161
signedav <david@opengis.ch>
2026-07-20 16:10:39 +0200

    bump toppingmaker

--- scripts/package_pip_packages.sh
+++ scripts/package_pip_packages.sh
@@ -2,7 +2,7 @@
 LIBS_DIR="modelbaker/libs"
 
 PGSERVICEPARSER=("pgserviceparser" "2.2.1")
-TOPPINGMAKER=("toppingmaker" "1.6.0")
+TOPPINGMAKER=("toppingmaker" "1.7.0")
 
 PACKAGES=(
   PGSERVICEPARSER[@]
